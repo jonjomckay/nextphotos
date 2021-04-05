c14ab696556b62807945a04a0ab980d53f389406
Jonjo McKay <jonjo@jonjomckay.com>
2021-04-05 22:55:28 +0100

    Fixed placeholder images in the library screen

--- lib/nextcloud/image.dart
+++ lib/nextcloud/image.dart
@@ -1,11 +1,5 @@
-import 'dart:async';
-import 'dart:developer';
-import 'dart:ui';
-
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:nextcloud/nextcloud.dart';
 import 'package:nextphotos/database/entities.dart';
 import 'package:nextphotos/home/home_model.dart';
 import 'package:nextphotos/photo/photo_page.dart';
@@ -35,58 +29,19 @@ class Pic extends StatelessWidget {
               }));
             },
             child: CachedNetworkImage(
+              cacheKey: photo.id,
               imageUrl: 'https://${model.hostname}/index.php/apps/files/api/v1/thumbnail/256/256$actualPath',
               httpHeaders: {
-                'Authorization': model.authorization, 'OCS-APIRequest': 'true'
+                'Authorization': model.authorization,
+                'OCS-APIRequest': 'true'
               },
               height: 256,
               width: 256,
               fit: BoxFit.cover,
-              placeholder: (context, url) => Image.asset('assets/images/placeholder.png'),
+              placeholder: (context, url) => Container(color: Colors.white10),
             )
         );
       },
     );
-  }
-}
-
-/// TODO: Cache these images: https://pub.dev/packages/cached_network_image
-class NextCloudImageProvider extends ImageProvider<NextCloudImageProvider> {
-  const NextCloudImageProvider(this.client, this.uri, this.width, this.height, {this.scale = 1.0})
-      : assert(uri != null),
-        assert(scale != null);
-
-  final NextCloudClient client;
-  final String uri;
-  final int width;
-  final int height;
-  final double scale;
-
-  @override
-  ImageStreamCompleter load(NextCloudImageProvider key, DecoderCallback decode) {
-    // Ownership of this controller is handed off to [_loadAsync]; it is that
-    // method's responsibility to close the controller's stream when the image
-    // has been loaded or an error is thrown.
-    final StreamController<ImageChunkEvent> chunkEvents = StreamController<ImageChunkEvent>();
-
-    return MultiFrameImageStreamCompleter(
-      codec: _loadAsync(decode),
-      chunkEvents: chunkEvents.stream,
-      scale: key.scale,
-      debugLabel: key.uri,
-    );
-  }
-
-  @override
-  Future<NextCloudImageProvider> obtainKey(ImageConfiguration configuration) {
-    return SynchronousFuture<NextCloudImageProvider>(this);
-  }
-
-  Future<Codec> _loadAsync(DecoderCallback decode) async {
-    try {
-      return decode(await client.preview.getThumbnail(uri, width, height));
-    } on RequestException {
-      log('Unable to load $uri');
-    }
   }
 }
